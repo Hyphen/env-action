a0d1a18a0f0fb750f890b18ada9a944cc22d00e5
Matthew Osborn <matthew@osbornm.com>
2024-10-18 08:57:03 -0500

    added hx file for testing

---
+++ .hx
@@ -0,0 +1,9 @@
+{
+  "project_name": "Pied Piper",
+  "project_id": "proj_6706929fd5f4bc6cc1724aa5",
+  "project_alternate_id": "pied-piper",
+  "app_name": "piperchat",
+  "app_id": "app_6706a6145a8f3e36ed062ef6",
+  "app_alternate_id": "piperchat",
+  "organization_id": "org_6706929ed5f4bc6cc1724a9f"
+}
